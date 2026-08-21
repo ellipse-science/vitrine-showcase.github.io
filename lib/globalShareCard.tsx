093de1bf66e99ca993f0eb46d723c3d08e4d5dd5
etienneprx <etienne.proulx.2@ulaval.ca>
2026-08-21 17:25:34 -0400

    fix(partage): reprendre les logos officiels
    Retire le monogramme créé pour la carte globale et compose l’identité avec les logos Vitrine et CAPP déjà utilisés dans l’en-tête.
    
    Assisté par : Codex (GPT-5)

--- lib/globalShareCard.tsx
+++ lib/globalShareCard.tsx
@@ -1,3 +1,6 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
 import { ImageResponse } from "next/og";
 
 import { loadShareFonts } from "@/lib/shareCardFonts";
@@ -5,8 +8,17 @@ import { CORDOVAN, INK, PAPER, RULE } from "@/lib/shareImageTokens";
 
 export const GLOBAL_SHARE_SIZE = { width: 1200, height: 630 };
 
+function pngDataUrl(bytes: Buffer): string {
+  return `data:image/png;base64,${bytes.toString("base64")}`;
+}
+
 export async function renderGlobalShareCard(): Promise<ImageResponse> {
-  const fonts = await loadShareFonts();
+  const brandDirectory = path.resolve(process.cwd(), "public", "images", "brand");
+  const [fonts, vitrineLogo, cappLogo] = await Promise.all([
+    loadShareFonts(),
+    fs.readFile(path.join(brandDirectory, "logo_vitrinedemocratique_bg-none_theme-black.png")),
+    fs.readFile(path.join(brandDirectory, "logo_capp_1row_bg-none_theme-black.png")),
+  ]);
 
   return new ImageResponse(
     <div
@@ -35,9 +47,61 @@ export async function renderGlobalShareCard(): Promise<ImageResponse> {
         style={{
           display: "flex",
           flexDirection: "column",
+          justifyContent: "center",
+          position: "relative",
+          width: "66%",
+          paddingRight: 46,
+          borderRight: `2px solid ${RULE}`,
+        }}
+      >
+        {/* Identité officielle : les deux PNG sont ceux du masthead du site. */}
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src={pngDataUrl(vitrineLogo)}
+          alt=""
+          width="670"
+          height="221"
+          style={{ width: "670px", height: "221px", objectFit: "contain" }}
+        />
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            marginTop: -24,
+            gap: 18,
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              fontFamily: "IBM Plex Mono",
+              fontSize: 14,
+              letterSpacing: 4,
+              textTransform: "uppercase",
+              whiteSpace: "nowrap",
+            }}
+          >
+            Propulsé par le
+          </div>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src={pngDataUrl(cappLogo)}
+            alt=""
+            width="365"
+            height="115"
+            style={{ width: "365px", height: "115px", objectFit: "contain" }}
+          />
+        </div>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
           justifyContent: "space-between",
           position: "relative",
-          width: "70%",
+          width: "34%",
+          paddingLeft: 46,
         }}
       >
         <div style={{ display: "flex", flexDirection: "column", gap: 12 }}>
@@ -46,102 +110,56 @@ export async function renderGlobalShareCard(): Promise<ImageResponse> {
             style={{
               display: "flex",
               fontFamily: "IBM Plex Mono",
-              fontSize: 17,
-              letterSpacing: 4.5,
+              fontSize: 15,
+              letterSpacing: 3.8,
               textTransform: "uppercase",
               opacity: 0.7,
             }}
           >
-            Centre d&apos;analyse des politiques publiques
+            Analyse en continu
           </div>
         </div>
 
-        <div style={{ display: "flex", flexDirection: "column", gap: 22 }}>
+        <div
+          style={{
+            display: "flex",
+            fontFamily: "Playfair Display",
+            fontWeight: 700,
+            fontSize: 34,
+            lineHeight: 1.18,
+          }}
+        >
+          Une lecture scientifique de la couverture médiatique et des discours politiques au Québec.
+        </div>
+
+        <div style={{ display: "flex", flexDirection: "column", gap: 20 }}>
           <div
             style={{
               display: "flex",
-              flexDirection: "column",
-              fontFamily: "Playfair Display",
-              fontWeight: 900,
-              fontSize: 78,
-              lineHeight: 0.92,
-              letterSpacing: -2.5,
+              flexWrap: "wrap",
+              gap: 12,
+              fontFamily: "IBM Plex Mono",
+              fontSize: 14,
+              letterSpacing: 2.4,
               textTransform: "uppercase",
             }}
           >
-            <span>La Vitrine</span>
-            <span>démocratique</span>
+            <span>Médias</span>
+            <span style={{ color: CORDOVAN }}>◆</span>
+            <span>Décideurs</span>
+            <span style={{ color: CORDOVAN }}>◆</span>
+            <span>Promesses</span>
           </div>
           <div
             style={{
               display: "flex",
-              maxWidth: 690,
-              fontSize: 27,
-              lineHeight: 1.25,
+              fontFamily: "IBM Plex Mono",
+              fontSize: 14,
+              letterSpacing: 1.5,
             }}
           >
-            Une lecture scientifique de la couverture médiatique et des discours politiques au Québec.
+            vitrinedemocratique.com
           </div>
-        </div>
-
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            gap: 16,
-            fontFamily: "IBM Plex Mono",
-            fontSize: 15,
-            letterSpacing: 2.6,
-            textTransform: "uppercase",
-          }}
-        >
-          <span>Médias</span>
-          <span style={{ color: CORDOVAN }}>◆</span>
-          <span>Décideurs</span>
-          <span style={{ color: CORDOVAN }}>◆</span>
-          <span>Promesses</span>
-        </div>
-      </div>
-
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          position: "relative",
-          width: "30%",
-          borderLeft: `2px solid ${RULE}`,
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            width: 236,
-            height: 236,
-            border: `5px solid ${CORDOVAN}`,
-            borderRadius: 118,
-            fontFamily: "Playfair Display",
-            fontWeight: 900,
-            fontSize: 176,
-            lineHeight: 1,
-            color: CORDOVAN,
-          }}
-        >
-          V
-        </div>
-        <div
-          style={{
-            display: "flex",
-            marginTop: 28,
-            fontFamily: "IBM Plex Mono",
-            fontSize: 14,
-            letterSpacing: 1.8,
-          }}
-        >
-          vitrinedemocratique.com
         </div>
       </div>
     </div>,
